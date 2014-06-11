eae3d48a0ad4ebd781a45ecbaa13b2f86aa99440
student <student@PC111S25.fgakusei.local>
2014-06-11 11:15:39 +0900

    後編

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -3,12 +3,18 @@ package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +37,47 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		btnOK.setOnClickListener(this);
 		Button btnMNT = (Button)findViewById(R.id.btnMNT);
 		btnMNT.setOnClickListener(this);
+		Button btnCHECK = (Button)findViewById(R.id.btnCHECK);
+		btnCHECK.setOnClickListener(this);
+
+		if(sdb == null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			return;
+	}
 
 	}
 	@Override
 	public void onClick(View v){
+		Intent intent = null;
 		switch(v.getId()){
-		case R.id.btnMNT:
-			//EditText etv = (EditText)findViewById(R.id.edtName);
-			//String inputMsg = etv.getText().toString();
-			Intent intent = null;
-			intent = new Intent(MainActivity.this, MntActivity.class);
-			startActivity(intent);
-			break;
+
 		case R.id.btnOK:
+			EditText etv = (EditText)findViewById(R.id.edtMsg);
+			String inputMsg = etv.getText().toString();
+			if(inputMsg!=null && !inputMsg.isEmpty()){
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+			etv.setText("");
 			intent = new Intent(MainActivity.this, HitokotoActivity.class);
 			startActivity(intent);
 			break;
+
+		case R.id.btnMNT:
+			intent = new Intent(MainActivity.this, MntActivity.class);
+			startActivity(intent);
+			break;
+
+		case R.id.btnCHECK:
+			String strHitokoto = helper.selectRandomHitokoto(sdb);
+			intent = new Intent(MainActivity.this, HitokotoActivity.class);
+			intent.putExtra("hitokoto",strHitokoto);
+			startActivity(intent);
+		break;
+
 
 			}
 
